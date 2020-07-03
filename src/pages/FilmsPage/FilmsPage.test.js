e95d05a6828497c25f66600e8ac5e3dc91418046
Marcelo Dapper <marcelo.dapper@gmail.com>
2020-07-03 15:22:41 -0300

    Refactor to use msw on tests (#3)
    * Add msw dependency
    * Add msw server config
    * Add msw server setup to run with jest
    * Fix to remove fetchMock from HomePage test
    * Fix to remove fetchMock from FilmsPage test
    * Fix to add test of error handling

--- src/pages/FilmsPage/FilmsPage.test.js
+++ src/pages/FilmsPage/FilmsPage.test.js
@@ -1,25 +1,11 @@
-/* eslint-disable no-undef */
 import React from 'react';
 import { Route } from 'react-router-dom';
 import { render, screen, waitForLoadingToFinish } from 'tests/app-test-utils';
-import { filmBuilder } from 'tests/factories/films';
-import { characterBuilder } from 'tests/factories/characters';
 
 import FilmsPage from './FilmsPage';
 
-const film = filmBuilder();
-
 describe('<FilmsPage />', () => {
-  beforeEach(() => {
-    fetchMock.resetMocks();
-  });
-
   it('renders', async () => {
-    fetchMock
-      .once(JSON.stringify({ ...film }), { status: 200 })
-      .once(JSON.stringify({ ...characterBuilder() }), { status: 200 })
-      .once(JSON.stringify({ ...characterBuilder() }), { status: 200 })
-      .once(JSON.stringify({ ...characterBuilder() }), { status: 200 });
     render(
       <Route path="/films/:id">
         <FilmsPage />
@@ -29,7 +15,7 @@ describe('<FilmsPage />', () => {
     await waitForLoadingToFinish();
 
     expect(screen.getAllByText(/FilmTitle/i)[0]).toBeInTheDocument();
-    expect(screen.getByText(/1/i)).toBeInTheDocument();
+    expect(screen.getByText(/Episode/i)).toBeInTheDocument();
     expect(screen.getByText(/DirectorName/i)).toBeInTheDocument();
     expect(screen.getByText(/ProducerName/i)).toBeInTheDocument();
     expect(screen.getAllByText(/CharacterName/i)[0]).toBeInTheDocument();
--- src/pages/HomePage/HomePage.test.js
+++ src/pages/HomePage/HomePage.test.js
@@ -1,23 +1,28 @@
-/* eslint-disable no-undef */
 import React from 'react';
 import { render, screen, waitForLoadingToFinish } from 'tests/app-test-utils';
-import { filmBuilder } from 'tests/factories/films';
+import { server, rest } from 'tests/server';
 
 import HomePage from './HomePage';
-
-const films = [filmBuilder(), filmBuilder()];
+const apiURL = process.env.REACT_APP_API_URL;
 
 describe('<HomePage />', () => {
-  beforeEach(() => {
-    fetchMock.resetMocks();
-  });
-
   it('renders', async () => {
-    fetchMock.once(JSON.stringify({ results: films }), { status: 200 });
     render(<HomePage />);
     await waitForLoadingToFinish();
 
     expect(screen.getAllByText(/FilmTitle/i)[0]).toBeInTheDocument();
     expect(screen.getAllByText(/FilmTitle/i)[1]).toBeInTheDocument();
+  });
+
+  it('fails the request and displays error', async () => {
+    server.use(
+      rest.get(`${apiURL}/films`, async (req, res, ctx) => {
+        return res(ctx.status(404));
+      })
+    );
+    render(<HomePage />);
+    await waitForLoadingToFinish();
+
+    expect(screen.getByText(/Error/i)).toBeInTheDocument();
   });
 });
