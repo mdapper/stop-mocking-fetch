6fc60c85519788512dce737310149db2cfcdaaf2
Marcelo Dapper <marcelo.dapper@gmail.com>
2020-07-02 21:47:54 -0300

    Add routes and pages to App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import FilmsPage from 'pages/FilmsPage';
+import HomePage from 'pages/HomePage';
 
 function App() {
   return (
-    <div>
-      <div>App</div>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route path="/films/:filmId">
+          <FilmsPage />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
