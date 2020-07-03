5e58d613a7dcbbbe41c341169bd067e16892176a
Marcelo Dapper <marcelo.dapper@gmail.com>
2020-07-03 12:10:38 -0300

    Fix to remove fetchMock from HomePage test

--- src/pages/HomePage/HomePage.test.js
+++ src/pages/HomePage/HomePage.test.js
@@ -1,19 +1,10 @@
-/* eslint-disable no-undef */
 import React from 'react';
 import { render, screen, waitForLoadingToFinish } from 'tests/app-test-utils';
-import { filmBuilder } from 'tests/factories/films';
 
 import HomePage from './HomePage';
 
-const films = [filmBuilder(), filmBuilder()];
-
 describe('<HomePage />', () => {
-  beforeEach(() => {
-    fetchMock.resetMocks();
-  });
-
   it('renders', async () => {
-    fetchMock.once(JSON.stringify({ results: films }), { status: 200 });
     render(<HomePage />);
     await waitForLoadingToFinish();
 
