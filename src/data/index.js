565bbc3ec523e550e84b5e5539613d91b352a6b9
Marcelo Dapper <marcelo.dapper@gmail.com>
2020-07-02 21:46:36 -0300

    Add some data with films posters

---
+++ src/data/index.js
@@ -0,0 +1,28 @@
+export const data = {
+  films: {
+    1: {
+      img:
+        'https://upload.wikimedia.org/wikipedia/en/8/87/StarWarsMoviePoster1977.jpg',
+    },
+    2: {
+      img:
+        'https://upload.wikimedia.org/wikipedia/pt/5/5c/The_Empire_Strikes_Back.jpg',
+    },
+    3: {
+      img:
+        'https://upload.wikimedia.org/wikipedia/en/b/b2/ReturnOfTheJediPoster1983.jpg',
+    },
+    4: {
+      img:
+        'https://upload.wikimedia.org/wikipedia/pt/thumb/3/30/Star_Wars_Epis%C3%B3dio_1_Amea%C3%A7a_Fantasma.jpg/240px-Star_Wars_Epis%C3%B3dio_1_Amea%C3%A7a_Fantasma.jpg',
+    },
+    5: {
+      img:
+        'https://upload.wikimedia.org/wikipedia/en/3/32/Star_Wars_-_Episode_II_Attack_of_the_Clones_%28movie_poster%29.jpg',
+    },
+    6: {
+      img:
+        'https://upload.wikimedia.org/wikipedia/pt/5/58/Star_Wars_Epis%C3%B3dio_III_A_Vingan%C3%A7a_dos_Sith.jpg',
+    },
+  },
+};
